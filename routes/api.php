26bb02f1ff792cc86c6becdcf014cdc99604ae81
zakriazakry <xxxzekoxxx57@gmail.com>
2025-11-17 01:26:29 +0200

    aa

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,15 @@
 use App\Http\Controllers\TelegramController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Log;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 Route::post('/bot/webhook', [TelegramController::class, 'handle']);
 Route::get('facebook/webhook', function (Request $request) {
+    Log::info($request->all());
+});
+Route::post('facebook/webhook', function (Request $request) {
     Log::info($request->all());
 });
