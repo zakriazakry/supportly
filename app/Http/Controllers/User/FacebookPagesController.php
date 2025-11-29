fd1484ac9f361d8bd39478665e34c4f010d2f6c6
zakriazakry <xxxzekoxxx57@gmail.com>
2025-11-29 19:25:17 +0200

    asd

--- app/Http/Controllers/User/FacebookPagesController.php
+++ app/Http/Controllers/User/FacebookPagesController.php
@@ -3,9 +3,27 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Services\FacebookLibsServices;
 use Illuminate\Http\Request;
 
 class FacebookPagesController extends Controller
 {
-    //
+    public $fb;
+    public function __construct()
+    {
+        $this->fb = new FacebookLibsServices();
+    }
+    public function index(Request $request)
+    {
+        $user = $request->user();
+        $accounts = $user->facebookAccounts;
+        $pages = collect([]);
+        foreach ($accounts as $account) {
+            $pages->push([
+                'account' => $account,
+                'pages' => $this->fb->getPages($account->access_token)
+            ]);
+        }
+        return responseFormat($pages->toArray(), 200);
+    }
 }
