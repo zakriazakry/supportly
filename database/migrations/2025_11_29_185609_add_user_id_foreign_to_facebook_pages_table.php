fcc50e480e561d212629d8df726bdf282e8f3ebd
zakriazakry <xxxzekoxxx57@gmail.com>
2025-11-29 20:56:21 +0200

    شسب

--- database/migrations/2025_11_29_185609_add_user_id_foreign_to_facebook_pages_table.php
+++ database/migrations/2025_11_29_185609_add_user_id_foreign_to_facebook_pages_table.php
@@ -12,7 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('facebook_pages', function (Blueprint $table) {
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
         });
     }
 
