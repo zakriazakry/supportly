679a054fe4e5ad0c5a783b7a29ea529c1393a8ff
zakriazakry <xxxzekoxxx57@gmail.com>
2025-11-29 17:44:13 +0200

    asf

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,16 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Services\FacebookLibsServices;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(AuthController::class)->prefix('auth')->group(function () {
     Route::post('login', 'login');
     Route::post('register', 'register');
 });
-
+Route::post('test', function (Request $request) {
+    $fb = new FacebookLibsServices();
+    return responseFormat($fb->exchangeLongLivedUserToken($request->token), 200);
+});
 require_once base_path('routes/apis/user.php');
