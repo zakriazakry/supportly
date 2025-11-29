337460551dc8899c41fbbacc2b73471593a030bd
zakriazakry <xxxzekoxxx57@gmail.com>
2025-11-29 21:00:27 +0200

    asd

--- database/migrations/2025_11_28_000003_create_facebook_pages_table.php
+++ database/migrations/2025_11_28_000003_create_facebook_pages_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('facebook_pages', function (Blueprint $table) {
             $table->id();
             $table->foreignId('facebook_account_id')->constrained('facebook_accounts')->onDelete('cascade');
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->onDelete('cascade');
             $table->string('page_id', 100);
             $table->string('name', 255);
             $table->text('image');
