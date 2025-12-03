5fb2387bca0da463921ece9e56fc16491a6da9d0
zakriazakry <xxxzekoxxx57@gmail.com>
2025-12-04 00:14:55 +0200

    as

--- routes/apis/whatsapp.php
+++ routes/apis/whatsapp.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\Evolution\WebhookController;
 use App\Http\Controllers\User\WhatsAppController;
+use App\Http\Controllers\Webhook\WebhookController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/evolution/webhook', [WebhookController::class, 'handle']);
