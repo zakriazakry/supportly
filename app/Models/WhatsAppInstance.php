6ac2c8079fff7c641da20f3617c7f402d4e511cd
zakriazakry <xxxzekoxxx57@gmail.com>
2025-12-03 09:35:53 +0200

    asf

--- app/Models/WhatsAppInstance.php
+++ app/Models/WhatsAppInstance.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class WhatsAppInstance extends Model
 {
-    //
+    protected $table = 'whats_app_instances';
+    protected $fillable = [
+        'user_id',
+        'instance_name',
+        'token',
+        'status',
+        'qr_code',
+    ];
 }
