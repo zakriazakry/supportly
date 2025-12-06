df911dc34c76130f8af86675060bf278f423010d
zakriazakry <xxxzekoxxx57@gmail.com>
2025-12-06 14:37:48 +0200

    a

--- app/Http/Controllers/Webhook/WebhookController.php
+++ app/Http/Controllers/Webhook/WebhookController.php
@@ -31,7 +31,6 @@ class WebhookController extends Controller
             $data = $request->all();
 
             Log::info(' -----data -----', $data);
-            return responseFormat('ok');
 
             // Get event type
             $event = $data['event'] ?? null;
