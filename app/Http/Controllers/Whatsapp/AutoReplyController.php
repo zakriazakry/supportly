dc8bc260381efba9cabfb3ad2e07ff95e06f735b
zakriazakry <xxxzekoxxx57@gmail.com>
2025-12-09 00:54:45 +0200

    asd

--- app/Http/Controllers/Whatsapp/AutoReplyController.php
+++ app/Http/Controllers/Whatsapp/AutoReplyController.php
@@ -619,13 +619,12 @@ class AutoReplyController extends Controller
      */
     public function whenReceiveTextMessage(array $data): void
     {
-        Log::info($data['pushName']);
-        return;
+
         $instanceName = $data['instanceName'] ?? null;
         $message = $data['message'] ?? null;
         $fromNumber = $data['form_number'] ?? null;
         $remoteJid = $data['remote_jid'] ?? null;
-        $pushName = $data['push_name'] ?? null;
+        $pushName = $data['pushName'] ?? null;
         $isGroup = str_contains($remoteJid ?? '', '@g.us');
         if (!$instanceName || !$fromNumber || !$message) {
             Log::warning('Missing required fields in WhatsApp webhook', $data);
