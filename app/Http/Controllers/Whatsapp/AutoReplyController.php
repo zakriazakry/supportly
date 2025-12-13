97f31fbfe3d340e92f6d65b17d8e3a92af7ae458
zakriazakry <xxxzekoxxx57@gmail.com>
2025-12-13 12:46:12 +0200

    asa

--- app/Http/Controllers/Whatsapp/AutoReplyController.php
+++ app/Http/Controllers/Whatsapp/AutoReplyController.php
@@ -648,8 +648,6 @@ class AutoReplyController extends Controller
         if ($userLimit <= ($instance->messagesCount($fromNumber) / 2) && $userLimit != null && $lastMessage->created_at->isToday()) {
             Log::error('User Limit limit reached', ['instance_name' => $instanceName, 'user_limit' => $userLimit, 'messages_count' => $instance->messagesCount($fromNumber), 'last_message' => $lastMessage->created_at->format('Y-m-d H:i:s'), 'from_number' => $fromNumber]);
             return;
-        } else {
-            Log::info('User Limit limit reached', ['instance_name' => $instanceName, 'user_limit' => $userLimit, 'messages_count' => $instance->messagesCount($fromNumber), 'last_message' => $lastMessage->created_at->format('Y-m-d H:i:s'), 'from_number' => $fromNumber]);
         }
 
         // معالجة الرد التلقائي (قواعد)
