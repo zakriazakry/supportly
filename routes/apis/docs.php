4192163d3fd62f7f849f5b64d21837b4fe4187ad
zakriazakry <xxxzekoxxx57@gmail.com>
2025-12-24 20:15:36 +0200

    a

--- routes/apis/docs.php
+++ routes/apis/docs.php
@@ -3,6 +3,10 @@
 use App\Http\Controllers\Docs\WhatsappDocsController;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(WhatsappDocsController::class)->middleware('auth:sanctum')->prefix('v1/whatsapp-developer')->group(function () {
+Route::controller(WhatsappDocsController::class)->prefix('v1/whatsapp-developer')->group(function () {
     Route::post('send-message', 'sendMessage');
+    Route::post('send-image', 'sendImage');
+    Route::post('send-video', 'sendVideo');
+    Route::post('send-audio', 'sendAudio');
+    Route::post('send-document', 'sendDocument');
 });
