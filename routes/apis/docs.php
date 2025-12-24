6c810b938fb90c78e0274d1335dc7a18ff2607f1
zakriazakry <xxxzekoxxx57@gmail.com>
2025-12-24 20:44:27 +0200

    a

--- routes/apis/docs.php
+++ routes/apis/docs.php
@@ -7,5 +7,5 @@ use Illuminate\Support\Facades\Route;
 Route::controller(WhatsappDocsController::class)->middleware(APIKeyVaildatorMiddleware::class)->prefix('v1/whatsapp-developer')->group(function () {
     Route::post('send-message', 'sendMessage');
     Route::post('show-write', 'showWirte');
-    Route::post('seen', 'seen');
+    Route::post('seen-message', 'seen');
 });
