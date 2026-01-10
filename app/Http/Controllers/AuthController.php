e9fc991e3e6fe8e4a9f6150d9a573d306d6a0bb5
zakriazakry <xxxzekoxxx57@gmail.com>
2026-01-10 22:10:18 +0200

    s

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -34,7 +34,7 @@ class AuthController extends Controller
         return responseFormat([
             'token' => $token,
             'user' => $user,
-            'permissions' => $user->getCurrentSubscription(),
+            'permissions' => $user->getCurrentSubscription()->package->getFeatures(),
         ], 200);
     }
 
