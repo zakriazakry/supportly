45924947f3cb199cbcb6c9b2fdc182735b7b392f
zakriazakry <xxxzekoxxx57@gmail.com>
2026-01-11 23:31:32 +0200

    س

--- routes/apis/docs.php
+++ routes/apis/docs.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Docs\WhatsappDocsController;
 use App\Http\Middleware\APIKeyVaildatorMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(WhatsappDocsController::class)->middleware([APIKeyVaildatorMiddleware::class, 'feature:whatsapp,whatsapp_developer'])->prefix('v1/whatsapp-developer')->group(function () {
+Route::controller(WhatsappDocsController::class)->middleware([APIKeyVaildatorMiddleware::class])->prefix('v1/whatsapp-developer')->group(function () {
     Route::post('send-message', 'sendMessage');
     Route::post('send-media', 'sendMedia');
     Route::post('show-write', 'showWirte');
